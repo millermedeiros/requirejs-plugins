bc79d26d640e55bf3806cf8e34c41463066f9a34
millermedeiros <miller@millermedeiros.com>
2011-11-17 13:53:50 -0200

    fix async plugin UID generation and add JSONP example. closes #1.

--- src/async.js
+++ src/async.js
@@ -1,10 +1,13 @@
 /** @license
  * RequireJS plugin for async dependency load like JSONP and Google Maps
  * Author: Miller Medeiros
- * Version: 0.1.0 (2011/03/23)
+ * Version: 0.1.1 (2011/11/17)
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
 define(function(){
+
+    var DEFAULT_PARAM_NAME = 'callback',
+        _uid = 0;
 
     function injectScript(src){
         var s, t;
@@ -15,9 +18,14 @@ define(function(){
     function formatUrl(name, id){
         var paramRegex = /!(.+)/,
             url = name.replace(paramRegex, ''),
-            param = (paramRegex.test(name))? name.replace(/.+!/, '') : 'callback'; //default param name is 'callback'
+            param = (paramRegex.test(name))? name.replace(/.+!/, '') : DEFAULT_PARAM_NAME;
         url += (url.indexOf('?') < 0)? '?' : '&';
         return url + param +'='+ id;
+    }
+
+    function uid() {
+        _uid += 1;
+        return '__async_req_'+ _uid +'__';
     }
 
     return{
@@ -25,7 +33,7 @@ define(function(){
             if(config.isBuild){
                 onLoad(null); //avoid errors on the optimizer
             }else{
-                var id = '__mm_asynch_req__'+ (+new Date());
+                var id = uid();
                 window[id] = onLoad; //create a global variable that stores onLoad so callback function can define new module after async load
                 injectScript(formatUrl(name, id));
             }
