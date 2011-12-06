2d2353932e3c37bd1fdfc5bff1b3672ee34fbeb2
millermedeiros <miller@millermedeiros.com>
2011-12-06 11:27:14 -0200

    add propertyParser and refactor goog and font plugin. changed font API to use the new arguments format.

--- src/font.js
+++ src/font.js
@@ -1,48 +1,30 @@
 /** @license
  * RequireJS plugin for loading web fonts using the WebFont Loader
  * Author: Miller Medeiros
- * Version: 0.1.1 (2011/11/28)
+ * Version: 0.2.0 (2011/12/06)
  * Released under the MIT license
  */
-define(['amd-utils/queryString/decodeQuery'], function (decodeQuery) {
+define(['propertyParser'], function (propertyParser) {
 
-    var rStringArray = /^\[([^\]]+)\]$/; // match "[foo,bar]" capturing "foo,bar"
+    var rParts = /^([^,]+),([^\|]+)\|?/;
 
     function parseName(name) {
         var data = {},
             vendors = name.split('|'),
             n = vendors.length,
-            cur;
+            match;
 
         while (n--) {
-            cur = decodeQuery(vendors[n]);
-            data[ cur.vendor ] = processData(cur);
+            match = rParts.exec(vendors[n]);
+            data[ match[1] ] = propertyParser.parseProperties(match[2]);
         }
         return data;
-    }
-
-    function processData(params) {
-        var key, val, arrMatch;
-
-        //used only as the key name of the object
-        delete params.vendor;
-
-        //typecast arrays
-        for (key in params) {
-            if (params.hasOwnProperty(key)) {
-                val = params[key];
-                arrMatch = typeof val === 'string'? rStringArray.exec(val) : null;
-                params[key] = arrMatch? arrMatch[1].split(',') : val;
-            }
-        }
-
-        return params;
     }
 
     // API
     return {
 
-        //example: font!vendor=google&families=[Tangerine,Cantarell,Yanone Kaffeesatz:700]
+        //example: font!google,families:[Tangerine,Cantarell,Yanone Kaffeesatz:700]
         load : function(name, req, onLoad, config){
             if (config.isBuild) {
                 onLoad(null); //avoid errors on the optimizer
--- src/goog.js
+++ src/goog.js
@@ -1,20 +1,20 @@
 /** @license
  * RequireJS plugin for loading Google Ajax API modules thru `google.load`
  * Author: Miller Medeiros
- * Version: 0.1.3 (2011/12/05)
+ * Version: 0.2.0 (2011/12/06)
  * Released under the MIT license
  */
-define(['async'], function () {
+define(['async', 'propertyParser'], function (async, propertyParser) {
 
-    var rParts = /^([^,]+)(?:,([^,]+))?(?:,\[([^\]]+)\])?/;
+    var rParts = /^([^,]+)(?:,([^,]+))?(?:,(.+))?/;
 
     function parseName(name){
         var match = rParts.exec(name),
             data = {
                 moduleName : match[1],
-                version : match[2] || '1',
-                packages : match[3]? match[3].split(',') : []
+                version : match[2] || '1'
             };
+        data.settings = propertyParser.parseProperties(match[3]);
         return data;
     }
 
@@ -23,13 +23,13 @@ define(['async'], function () {
             if (config.isBuild) {
                 onLoad(null); //avoid errors on the optimizer
             } else {
-                var data = parseName(name);
+                var data = parseName(name),
+                    settings = data.settings;
+
+                settings.callback = onLoad;
 
                 req(['async!'+ (document.location.protocol === 'https:'? 'https' : 'http') +'://www.google.com/jsapi'], function(){
-                    google.load(data.moduleName, data.version, {
-                        packages : data.packages,
-                        callback : onLoad
-                    });
+                    google.load(data.moduleName, data.version, settings);
                 });
             }
         }
