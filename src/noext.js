08bcd1fcbcd9eb533e9ab6e5d687579297374412
millermedeiros <miller@millermedeiros.com>
2011-12-07 16:07:37 -0200

    simplify noext plugin

--- src/noext.js
+++ src/noext.js
@@ -3,7 +3,7 @@
  * JSONP services and any other kind of resource that already contain a file
  * extension or that shouldn't have one (like dynamic scripts).
  * Author: Miller Medeiros
- * Version: 0.3.0 (2011/10/26)
+ * Version: 0.3.1 (2011/12/07)
  * Released under the MIT license
  */
 define(function(){
@@ -13,13 +13,13 @@ define(function(){
     //API
     return {
         load : function(name, req, onLoad, config){
-            var url = req.toUrl(name).replace(/\.js$/, '');
-            req([url], function(mod){
+            req([req.toUrl(name)], function(mod){
                 onLoad(mod);
             });
         },
         normalize : function(name, norm){
             //append query string to avoid adding .js extension
+            //needs to be on normalize otherwise it won't work after build
             name += (name.indexOf('?') < 0)? '?' : '&';
             return name + QUERY_PARAM +'=1';
         }
